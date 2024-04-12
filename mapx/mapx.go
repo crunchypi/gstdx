0804ad5de4bf0341550828131a679f596c6fab37
crunchypi <crunchypi@protonmail.com>
2024-04-12 10:45:23 +0200

    feat: add mapx.FilterFn + tests

--- mapx/mapx.go
+++ mapx/mapx.go
@@ -18,3 +18,39 @@ func New[K Key, V Val](kv ...Pair[K, V]) map[K]V {
 
 	return r
 }
+
+// FilterFn returns a func which filters 'm' using a given filter func.
+// Example:
+//
+//	m := FilterFn(map[int]int{1: 1, 2: 2, 3: 3})(
+//		func(p Pair[int, int]) bool {
+//			return p.K > 1
+//		},
+//	)
+//
+//	// m is map[int]int{2:2, 3:3}
+func FilterFn[K Key, V Val, M ~map[K]V](m M) func(rcv func(Pair[K, V]) bool) M {
+	return func(f func(Pair[K, V]) bool) (r M) {
+		if len(m) == 0 {
+			return map[K]V{}
+		}
+
+		if f == nil {
+			r := make(map[K]V, len(m))
+			for k, v := range m {
+				r[k] = v
+			}
+
+			return r
+		}
+
+		r = make(map[K]V, len(m))
+		for k, v := range m {
+			if f(Pair[K, V]{K: k, V: v}) {
+				r[k] = v
+			}
+		}
+
+		return r
+	}
+}
--- mapx/mapx_test.go
+++ mapx/mapx_test.go
@@ -31,3 +31,27 @@ func TestNewIdeal(t *testing.T) {
 
 	assertEq("r", map[int]int{1: 2, 2: 3}, New(p1, p2), func(s string) { t.Fatal(s) })
 }
+
+func TestFilterFnIdeal(t *testing.T) {
+	init := map[int]int{1: 2, 2: 3}
+	have := FilterFn(init)(func(p Pair[int, int]) bool { return p.K > 1 })
+	want := map[int]int{2: 3}
+
+	assertEq("r", want, have, func(s string) { t.Fatal(s) })
+}
+
+func TestFilterFnWithNilM(t *testing.T) {
+	init := *new(map[int]int)
+	have := FilterFn(init)(func(p Pair[int, int]) bool { return p.K > 1 })
+	want := map[int]int{}
+
+	assertEq("r", want, have, func(s string) { t.Fatal(s) })
+}
+
+func TestFilterFnWithNilF(t *testing.T) {
+	init := map[int]int{1: 2, 2: 3}
+	have := FilterFn(init)(nil)
+	want := map[int]int{1: 2, 2: 3}
+
+	assertEq("r", want, have, func(s string) { t.Fatal(s) })
+}
