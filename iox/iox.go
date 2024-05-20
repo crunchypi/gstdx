de56604296ed5b144c960366ed591caf7c1066fb
crunchypi <crunchypi@protonmail.com>
2024-05-20 13:25:56 +0200

    feat: add pkg iox with encoder, decoder + tests

---
+++ iox/iox.go
@@ -0,0 +1,59 @@
+package iox
+
+import (
+	"io"
+)
+
+// Encoder encodes values into binary form. It is used for converting io.Reader
+// and io.Writer into Reader and Writer (of this pkg), see funcs for docs.
+// Encoder uses pre-generics semantics for interoperability with pre-generics
+// packages. Some commonly used encoders are:
+//   - gob.NewEncoder(bytes.NewBuffer(nil))
+//   - json.NewEncoder(bytes.NewBuffer(nil))
+type Encoder interface {
+	Encode(e any) error
+}
+
+// EncoderImpl implements Encoder with it's Encode method by deferring to 'Impl'.
+// This is for convenience, as you may use functional implementation of Encoder
+// without defining a new type (that's done for you here).
+type EncoderImpl struct {
+	Impl func(e any) error
+}
+
+// Encode implements Encoder by deferring to the internal "Impl" func.
+// If the internal "Impl" is not set, an io.ErrClosedPipe will be returned.
+func (impl EncoderImpl) Encode(e any) error {
+	if impl.Impl == nil {
+		return io.ErrClosedPipe
+	}
+
+	return impl.Impl(e)
+}
+
+// Decoder decodes values from binary form. It is used for converting io.Reader
+// and io.Writer into Reader and Writer (of this pkg), see funcs for docs.
+// Decoder uses pre-generics semantics for interoperability with pre-generics
+// packages. Some commonly used encoders are:
+//   - gob.NewDecoder(bytes.NewBuffer(nil))
+//   - json.NewDecoder(bytes.NewBuffer(nil))
+type Decoder interface {
+	Decode(e any) error
+}
+
+// DecoderImpl implements Decoder with it's Decode method by deferring to 'Impl'.
+// This is for convenience, as you may use functional implementation of Decoder
+// without defining a new type (that's done for you here).
+type DecoderImpl struct {
+	Impl func(d any) error
+}
+
+// Decode implements Decoder by deferring to the internal "Impl" func.
+// If the internal "Impl" is not set, an io.EOF will be returned.
+func (impl DecoderImpl) Decode(d any) error {
+	if impl.Impl == nil {
+		return io.EOF
+	}
+
+	return impl.Impl(d)
+}
