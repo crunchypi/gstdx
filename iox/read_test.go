c37a3ca81799937a2d3552854dc4a170c8fdeeea
crunchypi <crunchypi@protonmail.com>
2024-05-23 12:49:47 +0200

    feat: add iox.ReadCloser, iox.ReadCloserImpl + tests

--- iox/read_test.go
+++ iox/read_test.go
@@ -22,3 +22,35 @@ func TestReaderImplReadWithoutImpl(t *testing.T) {
 	assertEq("err", io.EOF, err, func(s string) { t.Fatal(s) })
 	assertEq("val", 0, val, func(s string) { t.Fatal(s) })
 }
+
+func TestReadCloserImplReadIdeal(t *testing.T) {
+	rc := ReadCloserImpl[int]{}
+	rc.ImplR = func(ctx context.Context) (int, error) { return 1, nil }
+
+	val, err := rc.Read(nil)
+	assertEq("err", *new(error), err, func(s string) { t.Fatal(s) })
+	assertEq("val", 1, val, func(s string) { t.Fatal(s) })
+}
+
+func TestReadCloserImplReadWithoutImpl(t *testing.T) {
+	rc := ReadCloserImpl[int]{}
+
+	val, err := rc.Read(nil)
+	assertEq("err", io.EOF, err, func(s string) { t.Fatal(s) })
+	assertEq("val", 0, val, func(s string) { t.Fatal(s) })
+}
+
+func TestReadCloserImplCloseIdeal(t *testing.T) {
+	rc := ReadCloserImpl[int]{}
+	rc.ImplC = func() error { return nil }
+
+	err := rc.Close()
+	assertEq("err", *new(error), err, func(s string) { t.Fatal(s) })
+}
+
+func TestReadCloserImplCloseWithoutImpl(t *testing.T) {
+	rc := ReadCloserImpl[int]{}
+
+	err := rc.Close()
+	assertEq("err", *new(error), err, func(s string) { t.Fatal(s) })
+}
